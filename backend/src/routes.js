d24e32a7f3048c9eb4dfdded2bb9f935dd55a30d
Felipe Rosa <flp.far@gmail.com>
2020-03-27 22:46:08 -0300

    Add IncidentController to list and create incident

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
 const routes = express.Router();
 
@@ -8,5 +9,11 @@ routes.get("/ongs", OngController.index);
 
 //creates new ong
 routes.post("/ongs", OngController.create);
+
+//lists incidents
+routes.get("/incidents", IncidentController.index);
+
+//creates new incident
+routes.post("/incidents", IncidentController.create);
 
 module.exports = routes;
