d454e6663e3d178248893092feccdab765e1da54
Felipe Rosa <flp.far@gmail.com>
2020-03-27 23:42:43 -0300

    Add cors support for security

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
+
+app.use(cors());
+/* 
+-- Allow access only from myapp.com
+app.use(cors({
+  origin: 'https://myapp.com'
+}));
+ */
 app.use(express.json());
 app.use(routes);
 
